38080cda20ae9147396969d87bb056afbce76bcd
MelissaBeritan <melissaberitan896@gmil.com>
2026-03-26 15:49:51 -0400

    modified:   src/auth/pages/LoginPage.jsx

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 export function LoginPage() {
   const navigate = useNavigate();
+  const [password, setPassword] = useState('');
+  const [userName, setUserName] = useState('');
 
   const onLogin = () => {
+    console.log(password, ' ', userName);
     navigate('/');
+
   }
   return (
     <>
-      <div>
         <h2>Login</h2>
-        <button onClick={onLogin}>Login</button>
-        </div>
+        <form>
+          <input value={userName} onChange={(e)=>setUserName(e.target.value)} type="text" placeholder="Nombre de usuario" />
+          <input value={password} onChange={(e)=>setPassword(e.target.value)} type="text" placeholder="Contraseña"/>
+          <button onClick={onLogin} type="submit">Login</button>
+        </form>
+        
     </>
   )
 }
